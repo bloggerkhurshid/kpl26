d2cfc7a87e88d34e8b4485087b816e11129354ed
bloggerkhurshid <k2khurshid@gmail.com>
2026-09-14 13:39:13 +0530

    Catch exact throwable in players.php

--- backend/api/players.php
+++ backend/api/players.php
@@ -16,18 +16,29 @@ use Kpl\Utils\Response;
 $method = $_SERVER['REQUEST_METHOD'];
 $controller = new PlayerController();
 
-if ($method === 'GET') {
-    if (isset($_GET['id'])) {
-        $controller->show();
+try {
+    if ($method === 'GET') {
+        if (isset($_GET['id'])) {
+            $controller->show();
+        } else {
+            $controller->index();
+        }
+    } elseif ($method === 'POST') {
+        $controller->store();
+    } elseif ($method === 'PUT') {
+        $controller->update();
+    } elseif ($method === 'DELETE') {
+        $controller->destroy();
     } else {
-        $controller->index();
+        Response::error("Method not allowed", 405);
     }
-} elseif ($method === 'POST') {
-    $controller->store();
-} elseif ($method === 'PUT') {
-    $controller->update();
-} elseif ($method === 'DELETE') {
-    $controller->destroy();
-} else {
-    Response::error("Method not allowed", 405);
+} catch (\Throwable $t) {
+    http_response_code(500);
+    echo json_encode([
+        'error' => $t->getMessage(),
+        'file' => $t->getFile(),
+        'line' => $t->getLine(),
+        'trace' => $t->getTraceAsString()
+    ]);
+    exit();
 }
